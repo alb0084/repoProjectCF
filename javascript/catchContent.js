98608c4e94f3c1cbdbeb5211e0fecaea9eedf335
Alberto <alberto.cerea84@gmail.com>
2019-08-22 20:04:53 +0200

    Add content About me

--- javascript/catchContent.js
+++ javascript/catchContent.js
@@ -1,6 +1,12 @@
 var targetDiv = document.getElementById('content');
 
-
+const aboutMe=   `Hello,</br> my name is Alberto and i'm  software developer for 2 years...recently  
+                  i've approached some front-end tools such as angular,react,vue and surfing on the web
+                  i have found an Intensive  Bootcamp course regarding the lates technolgy for web development organized by F&C...and 
+                  about this Initiative, i think that could be a great opportunity to level up my skills with your support and
+                  to try a new great experience in London.
+                  I looking forward to talking again soon. 
+                  happy coding everybody :) </br>Alb0084 ` 
 
 function populateData(event){
   switch(event.getAttribute('value')){ 
@@ -9,7 +15,7 @@ function populateData(event){
       break;
     }
     case 'B':{
-      htmlContent = "content for B";
+      htmlContent =  `<h1>${aboutMe}</h1>`;
 break;
     }
   }
