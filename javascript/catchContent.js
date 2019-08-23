662b0875f7113fad76c57987518dad1ed11c6588
Alberto <alberto.cerea84@gmail.com>
2019-08-23 12:17:29 +0200

    Text correction && fix problem css relative to width

--- javascript/catchContent.js
+++ javascript/catchContent.js
@@ -1,10 +1,10 @@
 var targetDiv = document.getElementById('content');
 
-const aboutMe=   `Hello,</br> my name is Alberto and i'm  software developer for 2 years...recently  
+const aboutMe=   `Hello,</br> My name is Alberto and i am  software developer for 2 years. Recently  
                   i've approached some front-end tools such as angular,react,vue and surfing on the web
-                  i have found an Intensive  Bootcamp course regarding the lates technolgy for web development organized by F&C...and 
-                  about this Initiative, i think that could be a great opportunity to level up my skills with your support and
-                  to try a new great experience in London.
+                  i have found an Intensive  Bootcamp course regarding the lates technolgy for web development organized by F&C. 
+                  About this Initiative, i think that could be a great opportunity to level up my skills with your support and
+                  to try a new great experience for working in London.
                   I looking forward to talking again soon. 
                   happy coding everybody :) </br>Alb0084 ` 
 
