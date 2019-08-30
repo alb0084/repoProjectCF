cc1cb8dc45e8a0cc3d946f9e4454dc0b527e9ac1
Alberto <alberto.cerea84@gmail.com>
2019-08-30 16:51:01 +0200

    set dinamic content for carousel content && Abuot Me... section

--- javascript/routeContent.js
+++ javascript/routeContent.js
@@ -1,6 +1,30 @@
 //catch content from id in index.html
-var targetDiv = document.getElementById('content');
-document.getElementById("idCheckBox").checked=true;
+const targetDiv  = document.getElementById('content');
+var targetNode= targetDiv;
+
+
+  const resizeElements =(param)=>{
+    const shortSize= targetDiv 
+    const largeSize=targetDiv;
+    if (param.checked) {
+    shortSize.setAttribute('style','width:71%');
+      var observer= new MutationObserver(function(){
+        if(document.getElementById('carousel')!=null){
+         let largeSizeCarousel = document.getElementById('carousel');
+          largeSizeCarousel.setAttribute('style','width:100%;height:530px;');
+           }
+           else{
+             if(document.getElementById('aboutMe')!=null){
+              let largeSizeAboutMe=document.getElementById('aboutMe');
+              largeSizeAboutMe.setAttribute('style','width:100%;');
+              }
+           }});
+          observer.observe(targetNode,{attributes:true, childList:true, characterData: true});
+  } else {largeSize.setAttribute('style','width:90%');
+  }
+}
+      
+
 const aboutMe=   `<span style = " font-family: 'Dancing Script', cursive;" >
                   Hello ,</br> 
                   My name is Albert and i am  software developer for 2 years. Recently, i've approached some front-end tools 
@@ -12,16 +36,16 @@ const aboutMe=   `<span style = " font-family: 'Dancing Script', cursive;" >
                   happy coding everybody </span>:)
                   </br> <span style =" font-family: autograf_personal_use_onlyRg ;"> Albert Cerea </span>` 
 //function that re-render content taking the event from carousel/About Me  
-function populateData(event){
+ const populateData =(event)=> {
   switch(event.getAttribute('value')){ 
     case 'A':{
      htmlContent =`
      <div id="content" style="width:100%">
         <div id=subContent>
-            <h3 style= " text-align:center; font-family: cursive; margin-bottom: 0px; 
-            ">
-                  Carousel</h3>
-            <object style="width: 1070px; height:530px;" type="text/html" data="./carousel/carousel.html "></object>
+            <h2 style= "text-align:center; font-family: cursive; margin-bottom: 0px;">
+              Carousel
+            </h2>
+            <object id="carousel" style="width: 1070px; height:530px;" type="text/html" data="./carousel/carousel.html "></object>
         </div>
      </div>
      `;
@@ -31,12 +55,12 @@ function populateData(event){
       htmlContent = `  
       <div id="content" style="width:100%">
         <div id=subContent>
-                <h3 style=" text-align: center; font-family: cursive; margin: 0 0px 0 0;">
+                <h2 style=" text-align: center; font-family: cursive; margin: 0 0px 0 0;">
                   About Me... 
-                </h3>
-                <h1 style="width: 1056px;">
+                </h2>
+                <h2 id="aboutMe" style="width: 1056px;">
                   ${aboutMe}
-                </h1>  
+                </h2>  
             </div>   
       </div>      
             `
